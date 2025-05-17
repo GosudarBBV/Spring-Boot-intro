13a57369589afb6aceff87ad3eacf693c2853daf
Bohdan Buhrak <bohdan0410@gmail.com>
2025-05-17 20:34:16 +0300

    prepared string empty project

---
+++ src/main/java/mate/academy/string/boot/StringBootIntroApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.string.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBootIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringBootIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mate/academy/string/boot/StringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.string.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringBootIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
