953073c1bfa20c7319bbc5166013468c59a1df4c
Bohdan Buhrak <bohdan0410@gmail.com>
2025-05-19 22:06:50 +0300

    master

--- src/main/java/mate/academy/string/boot/StringBootIntroApplication.java
+++ src/main/java/mate/academy/string/boot/StringBootIntroApplication.java
@@ -1,32 +1,11 @@
 package mate.academy.string.boot;
 
-import mate.academy.string.boot.model.Book;
-import mate.academy.string.boot.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StringBootIntroApplication {
-	@Autowired
-	private BookService bookService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(StringBootIntroApplication.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			Book book1 = new Book();
-			book1.setAuthor("Kotlara");
-			book1.setPages(222);
-			bookService.save(book1);
-			System.out.printf("Lol", bookService.findAll());
-
-		};
-	}
-
 }
